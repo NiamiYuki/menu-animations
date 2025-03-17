48c7b026cdc4fe8775333a060bb7a8ece87eb033
Helena <ukil89@mail.ru>
2025-03-17 22:35:21 +0300

    add menu rotate

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function onClick() {
     const burger = document.querySelector('.burger');
     burger.classList.toggle('open');
+    burger.classList.toggle('rotated');
 
     const background = document.querySelector('.container');
     background.classList.toggle('opened');
+
   }
