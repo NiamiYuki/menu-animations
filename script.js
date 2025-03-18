686d019b5dde43efc09e20331dd955f699f520c5
Helena <ukil89@mail.ru>
2025-03-18 16:36:47 +0300

    add reverse animation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function onClick() {
     const burger = document.querySelector('.burger');
     burger.classList.toggle('open');
     burger.classList.toggle('rotated');
-
+    burger.classList.toggle('open_burger');
     const background = document.querySelector('.container');
     background.classList.toggle('opened');
 
