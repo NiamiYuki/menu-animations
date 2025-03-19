4ff057e236ed5e0b67da7b2cb633a347dfddd437
Helena <ukil89@mail.ru>
2025-03-19 16:29:57 +0300

    add logo, menu

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function onClick() {
-    const burger = document.querySelector('.burger');
-    burger.classList.toggle('open');
-    burger.classList.toggle('rotated');
-    burger.classList.toggle('open_burger');
-    const background = document.querySelector('.container');
-    background.classList.toggle('opened');
+  const burger = document.querySelector('.menu__burger_button');
+  const background = document.querySelector('.container');
+  const menuPopup = document.querySelector('.menu__popup');  
 
-    const leaves = document.querySelector('#leaves');
-    leaves.classList.toggle('none');
+  burger.classList.toggle('open');
+  burger.classList.toggle('rotated');
+  burger.classList.toggle('open_burger');
+  background.classList.toggle('opened');
+  menuPopup.classList.toggle('hidden');
+}
 
-  }
+function closeMenu() {
+  const burger = document.querySelector('.menu__burger_button');
+  const menuPopup = document.querySelector('.menu__popup');  
+  const background = document.querySelector('.container');
+
+  burger.classList.remove('open');
+  burger.classList.remove('rotated');
+  burger.classList.remove('open_burger');
+  background.classList.remove('opened');
+  menuPopup.classList.add('hidden');
+}
