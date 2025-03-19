f7ce8d56609bf270641977ee8a2cff7e42a1936b
Helena <ukil89@mail.ru>
2025-03-19 17:19:13 +0300

    add animation to close menu

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 function onClick() {
   const burger = document.querySelector('.menu__burger_button');
   const background = document.querySelector('.container');
-  const menuPopup = document.querySelector('.menu__popup');  
+  const menuPopup = document.querySelector('.menu__popup'); 
 
+    if (menuPopup.classList.contains('hidden')) {
+      menuPopup.classList.remove('hidden');
+    } 
+ 
   burger.classList.toggle('open');
   burger.classList.toggle('rotated');
   burger.classList.toggle('open_burger');
   background.classList.toggle('opened');
-  menuPopup.classList.toggle('hidden');
 }
 
 function closeMenu() {
   const burger = document.querySelector('.menu__burger_button');
-  const menuPopup = document.querySelector('.menu__popup');  
   const background = document.querySelector('.container');
 
   burger.classList.remove('open');
   burger.classList.remove('rotated');
   burger.classList.remove('open_burger');
   background.classList.remove('opened');
-  menuPopup.classList.add('hidden');
 }
